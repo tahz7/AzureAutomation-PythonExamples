5c2fc22a23962cc91fc63bf142428ba26f1fd1b9
Taj <tahzeem.taj@eulonml09528.global.root>
2017-10-31 16:57:16 +0000

    updated with changes

--- RunAs/azure-parallel-start-vm.py
+++ RunAs/azure-parallel-start-vm.py
@@ -7,18 +7,32 @@ Azure Python SDK documentation : http://azure-sdk-for-python.readthedocs.io/en/l
 """
 import sys
 import threading
+from datetime import datetime
+import calendar
+from collections import defaultdict
 
 import automationassets
 from azure.mgmt.compute import ComputeManagementClient
 
 
-# replace with your own resource group
-resource_group = ""
-if resource_group == "":
-    raise Exception("Please provide a resource group")
+def _args():
+    """ set arguments here """
+
+    start_stop = sys.argv[1] # param_arg1: start_vm or stop_vm
+    resource_groups = sys.argv[2:] # param arg2: rg1 rg2 rg3
+    exclude_vm = [] # param_arg3: !_vm1 !_vm2
+
+    for vm in sys.argv[2:]:
+        if vm.startswith('!_'):
+            resource_groups.remove(vm)
+            vm = vm[2:]
+            exclude_vm.append(vm)
+
+    return start_stop, resource_groups, exclude_vm
+
 
 def get_automation_runas_credential(runas_connection):
-    """ Returns credentials to authenticate against Azure resoruce manager """
+    """ Returns credentials to authenticate against Azure resource manager """
     from OpenSSL import crypto
     from msrestazure import azure_active_directory
     import adal
@@ -26,7 +40,8 @@ def get_automation_runas_credential(runas_connection):
     # Get the Azure Automation RunAs service principal certificate
     cert = automationassets.get_automation_certificate("AzureRunAsCertificate")
     pks12_cert = crypto.load_pkcs12(cert)
-    pem_pkey = crypto.dump_privatekey(crypto.FILETYPE_PEM, pks12_cert.get_privatekey())
+    pem_pkey = crypto.dump_privatekey(crypto.FILETYPE_PEM,
+                                      pks12_cert.get_privatekey())
 
     # Get run as connection information for the Azure Automation service principal
     application_id = runas_connection["ApplicationId"]
@@ -46,39 +61,63 @@ def get_automation_runas_credential(runas_connection):
     )
 
 
-def start_vm(compute_client, rgn, vm_name):
-    print vm_name + "Starting " + str(vm_name)
-    async_vm_start = compute_client.virtual_machines.start(rgn, vm_name)
-    async_vm_start.wait()
-    print vm_name + "Started " + str(vm_name)
+def get_compute_client():
+    """ Authenticate to Azure using the Azure Automation
+     RunAs service principal
+    """
+    runas_connection = automationassets.get_automation_connection(
+        "AzureRunAsConnection")
+    azure_credential = get_automation_runas_credential(runas_connection)
+
+    compute_client = ComputeManagementClient(
+        azure_credential,
+        str(runas_connection["SubscriptionId"])
+    )
+
+    return compute_client
+
+
+def stop_start_vm(compute_client, rgn, vm_name, action):
+    """ start or stop vm's """
+
+    print "{} {}...".format(action, vm_name)
+    if action == 'start_vm':
+        async_vm_status = compute_client.virtual_machines.start(rgn, vm_name)
+    else:
+        async_vm_status = compute_client.virtual_machines.power_off(rgn,
+                                                                    vm_name)
+    async_vm_status.wait()
+    print "{} Done".format(vm_name)
     sys.stdout.flush()
 
 
-def stop_vm(compute_client, rgn, vm_name):
-    print vm_name + "Stopping " + str(vm_name)
-    async_vm_stop = compute_client.virtual_machines.power_off(rgn, vm_name)
-    async_vm_stop.wait()
-    print vm_name + "Stopped " + str(vm_name)
-    sys.stdout.flush()
+# sort out arguments
+start_stop, resource_groups, exclude_vm = _args()
+
+# exit if it's the weekend as we don't need to start any vm
+today = datetime.today()
+if today.weekday() in [5, 6] and start_stop == 'start_vm':
+    raise SystemExit("It's {} which is the weekend, so not doing anything "
+                     "today!".format(calendar.day_name[today.weekday()]))
 
 
-# Authenticate to Azure using the Azure Automation RunAs service principal
-runas_connection = automationassets.get_automation_connection("AzureRunAsConnection")
-azure_credential = get_automation_runas_credential(runas_connection)
+compute_client = get_compute_client()
+# dict for rg mapped to their vm list
+vm_list = defaultdict(dict)
 
-compute_client = ComputeManagementClient(
-    azure_credential,
-    str(runas_connection["SubscriptionId"])
-)
+for rg in resource_groups:
+    rg_vm_list = compute_client.virtual_machines.list(resource_group_name=rg)
+    vm_list[rg] = [vm.name for vm in rg_vm_list if vm.name not in exclude_vm]
 
-vm_list = compute_client.virtual_machines.list(resource_group_name=resource_group)
-
+# restart vm's
 threads = []
-for vm in vm_list:
-    thread = threading.Thread(target=start_vm, args=[compute_client, resource_group, vm.name])
-    thread.start()
-    threads.append(thread)
-    sys.stdout.flush()
+for rg, vm_group in vm_list.iteritems():
+    for vm in vm_group:
+        thread = threading.Thread(target=stop_start_vm,
+        args=[compute_client, rg, vm, start_stop])
+        thread.start()
+        threads.append(thread)
+        sys.stdout.flush()
 
 print "Waiting on VMs operation to complete..."
 
